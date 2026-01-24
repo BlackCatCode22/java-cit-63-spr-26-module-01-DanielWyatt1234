d3bb7774a48593d05e0ca52af2f45bc718e0f5a0
DanielWyatt1234 <dcwyatt1@my.scccd.edu>
2026-01-24 11:41:53 -0800

    Add files via upload

---
+++ VarsAndMath.java
@@ -0,0 +1,14 @@
+// VarsAndMath.Java
+// 1/24/26
+public class VarsAndMath {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        double b = 12.5;
+        double sum = a + b;
+
+        System.out.println("Integer Math:");
+        System.out.println(a + " + " + b + " = " + sum);
+    }
+}
