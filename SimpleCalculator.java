f1585d2e118248ed2178dd9c6bd52857c31c4a70
DanielWyatt1234 <dcwyatt1@my.scccd.edu>
2026-01-24 17:05:40 -0800

    Add files via upload

---
+++ SimpleCalculator.java
@@ -0,0 +1,26 @@
+// Simple Calculator
+// 1/24/26
+
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double num1 = sc.nextDouble();
+        System.out.print("Enter second number: ");
+        double num2 = sc.nextDouble();
+
+        double sum = num1 + num2;
+        double difference = num1 - num2;
+        double product = num1 * num2;
+        double quotient = num1 / num2;
+
+        System.out.println("\n---Results---");
+        System.out.println(num1 + "+" + num2 + "=" + sum);
+        System.out.println(num1 + "-" + num2 + "=" + difference);
+        System.out.println(num1 + "*" + num2 + "=" + product);
+        System.out.println(num1 + "/" + num2 + "=" + quotient);
+    }
+}
